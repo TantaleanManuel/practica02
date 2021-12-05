abda628460f1a5a61ae9cbc5359cf6516f1e1882
TantaleanManuel <santiagowt@gmail.com>
2021-12-05 15:00:28 -0500

    prog 4 java

---
+++ M0100801HolaMundo.java
@@ -0,0 +1,12 @@
+public class M0100801HolaMundo {
+    public static void main(String[] args) {
+        {
+            String saludar="Hola Mundo desde Java";
+            System.out.println(saludar);
+            System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+            
+            int numero=10;
+            System.out.println("numero = " + numero);
+        }
+    }
+}
